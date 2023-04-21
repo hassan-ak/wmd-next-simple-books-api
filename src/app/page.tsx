b8ee6a53d4b5bb8fd90cf4d71aa9f42f47896f9c
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-04-21 22:00:45 +0500

    api frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import Link from 'next/link';
+import { ApiAuthentication } from '@/components/ApiAuthentication';
+import { Intro } from '@/components/Intro';
+import { EntryPoint } from '@/components/EntryPoint';
+import { EndPoints } from '@/components/EndPoints';
+import { Status } from '@/components/Status';
+import { ListBooks } from '@/components/ListBooks';
+import { SingleBook } from '@/components/SingleBook';
+import { PlaceOrder } from '@/components/PlaceOrder';
+import { AllOrders } from '@/components/AllOrders';
+import { SingleOrder } from '@/components/SingleOrder';
+import { DeleteOrder } from '@/components/DeleteOrder';
+import { UpdateOrder } from '@/components/UpdateOrder';
 
 export default function Home() {
-  return <div>Simple Book API</div>;
+  return (
+    <div className='m-3 rounded-lg border-2 border-gray-300 p-5 leading-8'>
+      <Intro />
+      <EntryPoint />
+      <EndPoints />
+      <Status />
+      <ListBooks />
+      <SingleBook />
+      <PlaceOrder />
+      <AllOrders />
+      <SingleOrder />
+      <UpdateOrder />
+      <DeleteOrder />
+      <ApiAuthentication />
+    </div>
+  );
 }
